003856e01e6d9beead8ea1f30a17e87da1e5a0ad
sejutiii <126340484+sejutiii@users.noreply.github.com>
2025-09-18 00:29:52 +0600

    committing

--- Backend/processing.py
+++ Backend/processing.py
@@ -3,14 +3,3 @@ import pandas as pd
 # Load the original CSV into a DataFrame for future processing
 df = pd.read_csv('food.csv')
 
-# Select the required columns: first two columns, 'Data.Household Weights.1st Household Weight', and 'Description'
-selected_columns = [
-	df.columns[0],
-	df.columns[1],
-	'Data.Household Weights(Gram)',
-	'Data.Household Weight Description'
-]
-df_selected = df[selected_columns]
-
-# Save the selected columns to a new CSV file
-df_selected.to_csv('food_selected.csv', index=False)
